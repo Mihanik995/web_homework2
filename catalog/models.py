f4a37f2d3f04b312aec58e9734eda7d59c7bebf4
user <user@example.com>
2024-03-17 21:13:03 +0200

    realise Category and Product models

--- catalog/models.py
+++ catalog/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='name')
+    description = models.TextField(verbose_name='description', **NULLABLE)
+
     def __str__(self):
-        return ''
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
 class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='name')
+    description = models.TextField(verbose_name='description', **NULLABLE)
+    image = models.ImageField(upload_to='catalog/', verbose_name='image', **NULLABLE)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, verbose_name='category')
+    price = models.IntegerField(verbose_name='price')
+    created_at = models.DateTimeField(verbose_name='created at')
+    updated_at = models.DateTimeField(verbose_name='updated at')
+
     def __str__(self):
-        return ''
+        return f'{self.name} ({self.price} RUB)'
 
     class Meta:
         verbose_name = 'product'
