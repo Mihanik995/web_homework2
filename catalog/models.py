377fa4e4d1da7fe2a0c7908dcc44d0206170099c
user <user@example.com>
2024-03-17 20:38:12 +0200

    add Category and Product models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    def __str__(self):
+        return ''
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+class Product(models.Model):
+    def __str__(self):
+        return ''
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
