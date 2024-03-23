1b7d1cfcb1aec0a005397d8b3321e6532e17c913
user <user@example.com>
2024-03-23 18:46:41 +0200

    local Product fixes

--- catalog/models.py
+++ catalog/models.py
@@ -20,8 +20,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='catalog/', verbose_name='image', **NULLABLE)
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE, verbose_name='category')
     price = models.IntegerField(verbose_name='price')
-    created_at = models.DateTimeField(verbose_name='created at', default=datetime.now(timezone.utc))
-    updated_at = models.DateTimeField(verbose_name='updated at', default=datetime.now(timezone.utc))
+    created_at = models.DateTimeField(verbose_name='created at', auto_now_add = True)
+    updated_at = models.DateTimeField(verbose_name='updated at', auto_now=True)
 
     def __str__(self):
         return f'{self.name} ({self.price} RUB)'
