da4e0be269007f61e4e90317fa41433317860d59
user <user@example.com>
2024-03-24 12:57:44 +0200

    add controller for products view

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 def home(request):
-    return render(request, 'main/home.html')
+    context = {'object_list': Product.objects.all()}
+    return render(request, 'main/home.html', context)
 
 def contacts(request):
     if request.method == 'POST':
@@ -10,3 +14,7 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} ({phone}): "{message}"')
     return render(request, 'main/contacts.html')
+
+def products(request, pk):
+    context = {'object': Product.objects.get(pk=pk)}
+    return render(request, 'main/products.html', context)
