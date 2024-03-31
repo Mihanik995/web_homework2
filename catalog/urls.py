7a17e61334a3fa291da27e00ca8e91fe753d5bb7
user <user@example.com>
2024-03-31 19:45:58 +0300

    add CBVs for Publication

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from catalog.views import ProductDetailView, ProductListView, ContactsView
+from catalog.views import ProductDetailView, ProductListView, ContactsView, PublicationDetailView, \
+    PublicationCreateView, PublicationListView, PublicationUpdateView, PublicationDeleteView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactsView.as_view(), name='contacts_view'),
-    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail')
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('blog/', PublicationListView.as_view(), name='publication_list'),
+    path('blog/<int:pk>', PublicationDetailView.as_view(), name='publication_detail'),
+    path('blog/new_post/', PublicationCreateView.as_view(), name='add_publication'),
+    path('blog/<int:pk>/edit', PublicationUpdateView.as_view(), name='update_publication'),
+    path('blog/<int:pk>/delete', PublicationDeleteView.as_view(), name='delete_publication'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, DeleteView, UpdateView, CreateView
 
-from catalog.models import Product
+from catalog.models import Product, Publication
 
 
 class ProductListView(ListView):
@@ -12,6 +13,36 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     template_name = 'main/products.html'
     model = Product
+
+
+class PublicationCreateView(CreateView):
+    template_name = 'main/blog_form.html'
+    model = Publication
+    fields = ('title', 'text',)
+    success_url = reverse_lazy('publication_list')
+
+
+class PublicationListView(ListView):
+    template_name = 'main/blog_view.html'
+    model = Publication
+
+
+class PublicationDetailView(DetailView):
+    template_name = 'main/blog_detail.html'
+    model = Publication
+
+
+class PublicationUpdateView(UpdateView):
+    template_name = 'main/blog_form.html'
+    model = Publication
+    fields = ('title', 'text', 'preview',)
+    success_url = reverse_lazy('publication_list')
+
+
+class PublicationDeleteView(DeleteView):
+    template_name = 'main/blog_delete.html'
+    model = Publication
+    success_url = reverse_lazy('publication_list')
 
 
 class ContactsView(TemplateView):
