8828eb3985da4e92562c79dc58df3a514ff91fb4
user <user@example.com>
2024-04-26 22:15:30 +0300

    add login, logout and register functionals

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from catalog.models import NULLABLE
+
+
 class User(AbstractUser):
     username = None
 
-    avatar = models.ImageField(upload_to='users/', verbose_name='avatar')
-    phone = models.IntegerField(verbose_name='phone number')
-    country = models.CharField(max_length=50, verbose_name='country')
+    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', **NULLABLE)
+    phone = models.IntegerField(verbose_name='phone number', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='country', **NULLABLE)
 
     email = models.EmailField(verbose_name='e-mail', unique=True)
     USERNAME_FIELD = 'email'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import CustomUserCreationForm
+from users.models import User
+
+
+class UserRegisterView(CreateView):
+    template_name = 'users/register.html'
+    model = User
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('register')
