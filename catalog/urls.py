fa4a976f6ea36bf8a62c08b7ddbb5af2f0737a6d
user <user@example.com>
2024-05-02 21:36:11 +0300

    cache product detail template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.views import ProductDetailView, ProductListView, ContactsView, PublicationDetailView, \
     PublicationCreateView, PublicationListView, PublicationUpdateView, PublicationDeleteView, ProductCreateView, \
@@ -7,7 +8,7 @@ from catalog.views import ProductDetailView, ProductListView, ContactsView, Publ
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactsView.as_view(), name='contacts_view'),
-    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('products/<int:pk>', cache_page(300)(ProductDetailView.as_view()), name='product_detail'),
     path('products/new_product', ProductCreateView.as_view(), name='add_product'),
     path('products/<int:pk>/update', ProductUpdateView.as_view(), name='update_product'),
     path('blog/', PublicationListView.as_view(), name='publication_list'),
